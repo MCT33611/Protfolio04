2221bc3832c0a48fc8274a6d4161eaa73d56bff8
muhammed fahmi <muhammedfahmi04@gmail.com>
2025-08-03 01:14:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.spec-india.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
